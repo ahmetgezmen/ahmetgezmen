fe06620e5516bc14e71adb4865e1a11625dc007a
Ahmet GEZMEN <ahmet_gezmen_@hotmail.com>
2023-04-28 23:44:45 +0300

    provider scope added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:ahmetgezmen/core/base/app_base.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp(),),);
 }
 
